ecbba5c30b0f10dd15a31567f62afce0b33cc1c0
madking55 <madking55@gmail.com>
2020-03-28 12:06:36 +0100

    Add Turn and TurnTest / finish iteration 2

--- 1module/projects/war_or_peace/test/turn_test.rb
+++ 1module/projects/war_or_peace/test/turn_test.rb
@@ -28,6 +28,34 @@ class TurnTest < Minitest::Test
     assert_equal [], turn.spoils_of_war
   end
 
+  def test_it_adds_cards_from_spoils_to_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new('Megan', deck1)
+    player2 = Player.new('Aurora', deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+
+    assert_equal :basic, turn.type
+    assert_equal player1, winner
+
+    turn.pile_cards
+    assert_equal [card8, card7], turn.spoils_of_war
+    turn.award_spoils(winner)
+  
+    assert_equal [card1, card2, card5, card7, card8], winner.deck.cards
+    assert_equal [], turn.spoils_of_war
+  end
+
+  # Tests for :basic type
   def test_it_is_a_basic_type
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
@@ -83,10 +111,13 @@ class TurnTest < Minitest::Test
     assert_equal :basic, type
 
     turn.pile_cards
+    assert_equal [card1], player1.deck
+    assert_equal [card3], player2.deck
     assert_equal [card8, card7], turn.spoils_of_war
   end
 
-  def test_it_adds_cards_from_spoils_to_winner
+  # Tests for :war type
+  def it_has_war_type
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -96,20 +127,111 @@ class TurnTest < Minitest::Test
     card7 = Card.new(:heart, '3', 3)
     card8 = Card.new(:diamond, '2', 2)
     deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card3, card4, card6, card7])
-    player1 = Player.new('Megan', deck1)
-    player2 = Player.new('Aurora', deck2)
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
-    winner = turn.winner
 
-    assert_equal :basic, turn.type
-    assert_equal player1, winner
+    assert_equal :war, turn.type
+  end
 
+  def it_has_winner_for_war_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :war, turn.type
+    assert_equal player2, turn.winner
+  end
+
+  def it_has_pile_of_card_for_war_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :war, turn.type
+    turn.pile_cards
+    assert_equal [card1], player1.deck
+    assert_equal [card4], player2.deck
+    assert_equal [card2, card5, card8, card4, card3, card6, card7], turn.spoils_of_war
+
+  end
+
+  # Tests for :mutually_assured_destruction type
+  def it_has_mutually_assured_destruction_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :mutually_assured_destruction, turn.type
+  end
+
+  def it_has_pile_of_cards_for_mutually_assured_destruction_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :mutually_assured_destruction, turn.type
     turn.pile_cards
     assert_equal [card8, card7], turn.spoils_of_war
-    turn.award_spoils(winner)
-  
-    assert_equal [card1, card2, card5, card7, card8], winner.deck.cards
-    assert_equal [], turn.spoils_of_war
+  end
+
+  def it_has_winner_for_mutually_assured_destruction_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :mutually_assured_destruction, turn.type
+    assert_equal 'No Winner', turn.winner
   end
 end
