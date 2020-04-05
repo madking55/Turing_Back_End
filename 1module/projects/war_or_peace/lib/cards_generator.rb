71b092c2072cd8c2f45ecfe582419f59663a0bc8
madking55 <madking55@gmail.com>
2020-04-05 22:09:06 +0200

    WIP Iterartion 4 card_generator

--- 1module/projects/war_or_peace/lib/cards_generator.rb
+++ 1module/projects/war_or_peace/lib/cards_generator.rb
@@ -1,17 +1,36 @@
 class CardsGenerator
-  attr_reader :filename
+  attr_reader :filename, :game_deck
 
   def initialize(filename)
     @filename = filename
+    @game_deck = []
+  end
+
+
+  def import_from_file
+    file = File.open(filename).each { |card| @game_deck << card }
+    file.close
+  end
+
+  # def transform_to_card
+  #   cards = []
+  #   @game_deck.each do |line|
+  #     # element => Card.new(suit, value, rank)
+  #     transformed_line = line.strip.split(', ') # => ["Ace", "Club", "14"]
+  #     card = Card.new(transformed_line[1].downcase.to_sym, transformed_line[0], transformed_line[2].to_i)
+  #     cards << card
+  #   end
+  #   cards
+  # end
+
+  def transform_line_to_card
+      # line => Card.new(suit, value, rank)
+      transformed_line = line.strip.split(', ') # => ["Ace", "Club", "14"]
+      Card.new(transformed_line[1].downcase.to_sym, transformed_line[0], transformed_line[2].to_i)
   end
 
   def cards
-    game_deck = []
-    # card = Card.new(suit, value, rank)
-    file = File.open(filename).each { |card| game_deck << card }
-    file.close
-    end
+    import_from_file
+    @game_deck.map(&:transform_line_to_card)
   end
 end
-
-# p CardsGenerator.new('cards.txt').cards
--- 1module/projects/war_or_peace/test/cards_generator_test.rb
+++ 1module/projects/war_or_peace/test/cards_generator_test.rb
@@ -11,10 +11,23 @@ require 'pry'
 class CardsGeneratorTest < Minitest::Test
 
   def test_it_exists
-    filename = 'cards.txt'
-    cards = CardsGenerator.new(filename).cards
-    assert_instance_of CardsGenerator, cards
-    
+    cards_generator = CardsGenerator.new('cards.txt')
+    assert_instance_of CardsGenerator, cards_generator
   end
-  
+
+  def test_it_imports_from_file
+    cards_generator = CardsGenerator.new('cards.txt')
+    assert_equal [], cards_generator.game_deck
+
+    cards_generator.import_from_file
+    assert_equal 52, cards_generator.game_deck.size
+  end
+
+  def test_it_transforms_elements_of_deck_into_cards
+    cards_generator = CardsGenerator.new('cards.txt')
+    cards_generator.import_from_file
+    
+    assert cards_generator.cards.all?(Card)
+  end
+
 end
