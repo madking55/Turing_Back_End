98063720bdbed08e338bed6604ea1743627f3443
madking55 <madking55@gmail.com>
2020-04-14 11:45:34 +0200

    Finish Modules in classwork

---
+++ 1module/classwork/ruby-module-example/status_update.rb
@@ -0,0 +1,21 @@
+require './commentable'
+
+class StatusUpdate
+	include Commentable
+	attr_reader :body, :comments
+
+	def initialize(body)
+		@body = body
+		@comments = {}
+	end
+
+	def display
+		puts "-" * 50
+		puts "STATUS UPDATE"
+		puts "Body: #{body}"
+		comments.each do |id, text|
+			puts "#{id}: #{text}"
+		end
+		puts "-" * 50
+	end
+end
