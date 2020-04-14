50edc8dde40de549ecf3c4bc15ee46c4d62044da
madking55 <madking55@gmail.com>
2020-04-14 10:16:52 +0200

    WIP Iteration 1

--- 1module/projects/battleship/test/cell_test.rb
+++ 1module/projects/battleship/test/cell_test.rb
@@ -5,6 +5,17 @@ require './lib/cell'
 require './lib/ship'
 
 class CellTest < Minitest::Test
+
+  def test_it_exists
+    cell = Cell.new("B4")
+    assert_instance_of Cell, cell
+  end
+
+  def test_it_has_coordinate
+    cell = Cell.new("B4")
+    assert_equal "B4", cell.coordinate
+  end
+
   
 
 end
