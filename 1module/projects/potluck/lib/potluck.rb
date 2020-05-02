d5c14720ed70fbb14acc95c5fa4b29ef54cd8c8b
madking55 <madking55@gmail.com>
2020-05-02 21:21:05 +0200

    Finish Potluck project

--- 1module/projects/potluck/lib/potluck.rb
+++ 1module/projects/potluck/lib/potluck.rb
@@ -12,4 +12,21 @@ class Potluck
   def get_all_from_category(category)
     @dishes.select { |dish| dish.category == category }
   end
+
+  def menu
+    menu = {}
+    @dishes.each do |dish|
+      if menu[(dish.category.to_s + 's').to_sym].nil?
+      menu[(dish.category.to_s + 's').to_sym] = [dish.name]
+      else
+      menu[(dish.category.to_s + 's').to_sym] << dish.name
+      end
+    end
+    menu.each { |category, names| names.sort! }
+  end
+
+  def ratio(category)
+    (get_all_from_category(category).length / dishes.length.to_f) * 100
+  end
+  
 end
--- 1module/projects/potluck/test/potluck_test.rb
+++ 1module/projects/potluck/test/potluck_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 require './lib/dish'
 require './lib/potluck'
 
@@ -46,5 +47,44 @@ class PotluckTest < Minitest::Test
     assert_equal [couscous_salad, summer_pizza], potluck.get_all_from_category(:appetizer)
     assert_equal couscous_salad, potluck.get_all_from_category(:appetizer).first
     assert_equal "Couscous Salad", potluck.get_all_from_category(:appetizer).first.name
+  end
+
+  def test_it_displays_menu
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    potluck.add_dish(bean_dip)
+
+    expected = {:appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],:entres=>["Cocktail Meatballs", "Roast Pork"],:desserts=>["Candy Salad"]} 
+    assert_equal expected, potluck.menu
+  end
+
+  def test_it_count_ratio_by_category
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    potluck.add_dish(bean_dip)
+
+    assert_equal 50.0, potluck.ratio(:appetizer)
   end
 end
