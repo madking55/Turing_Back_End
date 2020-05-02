a477625bbdc747ae69cd3bc0966ae16210796daf
madking55 <madking55@gmail.com>
2020-05-02 20:17:13 +0200

    Finish Iteration 3

--- 1module/projects/potluck/test/potluck_test.rb
+++ 1module/projects/potluck/test/potluck_test.rb
@@ -23,10 +23,28 @@ class PotluckTest < Minitest::Test
     potluck.add_dish(couscous_salad)
 
     assert_equal [couscous_salad], potluck.dishes
-    
+
     potluck.add_dish(cocktail_meatballs)
     
     assert_equal [couscous_salad, cocktail_meatballs], potluck.dishes
     assert_equal 2, potluck.dishes.length
+  end
+
+  def test_it_lists_dishes_by_category
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+
+    assert_equal [couscous_salad, summer_pizza], potluck.get_all_from_category(:appetizer)
+    assert_equal couscous_salad, potluck.get_all_from_category(:appetizer).first
+    assert_equal "Couscous Salad", potluck.get_all_from_category(:appetizer).first.name
   end
 end
