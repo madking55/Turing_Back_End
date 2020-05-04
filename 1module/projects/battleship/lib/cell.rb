6a5339a93aba5fa4da35ac8c61ee906f9d3a35a5
madking55 <madking55@gmail.com>
2020-05-04 22:20:07 +0200

    Finish Iteration 1

--- 1module/projects/battleship/lib/cell.rb
+++ 1module/projects/battleship/lib/cell.rb
@@ -2,5 +2,37 @@ class Cell
   attr_reader :coordinate
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
+    @fired_upon = false
   end
+
+  def ship
+    @ship
+  end
+
+  def empty?
+    ship.nil?
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    ship.hit unless empty? 
+    @fired_upon = true
+  end
+
+  def render(arg = false)
+    return "S" if arg == true && !fired_upon?
+    return "." unless fired_upon?
+    return "M" if fired_upon? && empty? 
+    return "X" if fired_upon? && !empty? && ship.sunk?
+    return "H" if fired_upon? && !empty?
+  end
+
 end
--- 1module/projects/battleship/test/cell_test.rb
+++ 1module/projects/battleship/test/cell_test.rb
@@ -16,6 +16,48 @@ class CellTest < Minitest::Test
     assert_equal "B4", cell.coordinate
   end
 
-  
+  def test_it_can_be_empty
+    cell = Cell.new("B4")
+    assert_nil cell.ship
+    assert cell.empty?
+  end
+
+  def test_it_contains_ship
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    assert_equal cruiser, cell.ship
+    refute cell.empty?
+  end
+
+  def test_it_knows_when_fired_upon
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    refute cell.fired_upon?
+    cell.fire_upon
+    assert_equal 2, cell.ship.health
+    assert cell.fired_upon?
+  end
+
+  def test_it_renders_cell_string_representation
+    cell_1 = Cell.new("B4")
+    assert_equal ".", cell_1.render
+    cell_1.fire_upon
+    cell_1.render
+    assert_equal "M", cell_1.render
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    assert_equal ".", cell_2.render
+    assert_equal "S", cell_2.render(true)
+    cell_2.fire_upon
+    assert_equal "H", cell_2.render
+    refute cruiser.sunk?
+    cruiser.hit
+    cruiser.hit
+    assert cruiser.sunk?
+    assert_equal "X", cell_2.render
+  end
 
 end
