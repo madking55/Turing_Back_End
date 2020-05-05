bced85a080689a369501ad29e27282d457a8c88f
madking55 <madking55@gmail.com>
2020-05-05 21:13:33 +0200

    WIP Iteration 2 - Validating Coordinates

---
+++ 1module/projects/battleship/lib/board.rb
@@ -0,0 +1,59 @@
+class Board
+  attr_reader :cells
+
+  def initialize
+    @cells = {
+    "A1" => Cell.new("A1"),
+    "A2" => Cell.new("A2"),
+    "A3" => Cell.new("A3"),
+    "A4" => Cell.new("A4"),
+    "B1" => Cell.new("B1"),
+    "B2" => Cell.new("B2"),
+    "B3" => Cell.new("B3"),
+    "B4" => Cell.new("B4"),
+    "C1" => Cell.new("C1"),
+    "C2" => Cell.new("C2"),
+    "C3" => Cell.new("C3"),
+    "C4" => Cell.new("C4"),
+    "D1" => Cell.new("D1"),
+    "D2" => Cell.new("D2"),
+    "D3" => Cell.new("D3"),
+    "D4" => Cell.new("D4")
+    }
+  end
+
+  def vaild_coordinate?(cell)
+    @cells.keys.include?(cell)
+  end
+
+  def valid_placement?(ship, coordinates)
+    coordinates_and_ship_size_equal?(ship, coordinates) && 
+    consecutive_coordinates?(ship, coordinates)
+  end
+
+  def coordinates_and_ship_size_equal?(ship, coordinates)
+    ship.length == coordinates.count
+  end
+
+  def consecutive_coordinates?(ship, coordinates)
+    horizontal_check(ship, coordinates) || vertical_check(ship, coordinates)
+  end
+
+  def horizontal_check(ship, coordinates)
+
+    # find the first cell in coordinates
+    c = coordinates[0]  
+
+    # find index of c in cells
+    index_of_c_in_cells = @cells.keys.index(coordinates[0]) 
+
+    # check if coordinates array is equal to array in rows starting from c
+    coordinates == @cells.keys[index_of_c_in_cells...coordinates.size + index_of_c_in_cells]
+  end
+
+  def vertical_check
+    # row_size = Math.sqrt(@cells.size).to_i
+
+  end
+
+end
---
+++ 1module/projects/battleship/test/board_test.rb
@@ -0,0 +1,88 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/cell'
+require './lib/ship'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+
+  def test_it_exists
+    board = Board.new
+    assert_instance_of Board, board
+  end
+
+  def test_it_has_16_cells
+    board = Board.new
+    assert_instance_of Hash, board.cells
+    assert_equal 16, board.cells.length
+    assert board.cells.values.all? { |cell| cell.is_a?(Cell) }
+  end
+
+  def test_it_can_validate_cell_coordinates
+    board = Board.new
+    assert board.vaild_coordinate?("A1")
+    assert board.vaild_coordinate?("D4")
+    refute board.vaild_coordinate?("A5")
+    refute board.vaild_coordinate?("E1")
+    refute board.vaild_coordinate?("A22")
+  end
+
+  def test_it_can_validate_ship_placement
+    skip
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2) 
+
+    assert board.valid_placement?(submarine, ["A1", "A2"])
+    assert board.valid_placement?(cruiser, ["B1", "C1", "D1"])
+  end
+
+  def test_coordinates_and_ship_size_equal
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2) 
+
+    refute board.coordinates_and_ship_size_equal?(cruiser, ["A1", "A2"])
+    refute board.coordinates_and_ship_size_equal?(submarine, ["A2", "A3", "A4"])
+  end
+
+  def test_consecutive_coordinates?
+    skip
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2) 
+    # binding.pry
+
+    # refute board.consecutive_coordinates?(cruiser, ["A1", "A2", "A4"])
+    # refute board.consecutive_coordinates?(submarine, ["A1", "C1"])
+    # refute board.consecutive_coordinates?(cruiser, ["A3", "A2", "A1"])
+    # refute board.consecutive_coordinates?(submarine, ["C1", "B1"])
+    assert board.consecutive_coordinates?(cruiser, ["A2", "A3", "A4"])
+
+    # assert board.consecutive_coordinates?(submarine, ["A1", "B1"])
+    # assert board.consecutive_coordinates?(cruiser, ["A3", "B3", "C3"])
+  end
+
+  def test_horizontal_consecutive_coordinates
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2) 
+
+    refute board.horizontal_check(cruiser, ["A1", "A2", "A4"])
+    assert board.horizontal_check(cruiser, ["A2", "A3", "A4"])
+    assert board.horizontal_check(submarine, ["A1", "A2"])
+  end
+
+  def test_coordinates_not_diagonal
+    skip
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2) 
+
+    refute board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+    refute board.valid_placement?(submarine, ["C2", "D3"])
+  end
+
+  
+end
