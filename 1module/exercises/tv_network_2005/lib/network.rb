435280f18e1d20771d2f61174aa8e1cbf1a38740
madking55 <madking55@gmail.com>
2020-05-24 17:24:13 +0200

    Finish Iteration 4

--- 1module/exercises/tv_network_2005/lib/network.rb
+++ 1module/exercises/tv_network_2005/lib/network.rb
@@ -16,10 +16,24 @@ class Network
 
   def actors_by_show
     actors_by_show = {}
-    @shows.each do |show|
-     actors_by_show[show] = show.actors
-    end
+    @shows.each { |show| actors_by_show[show] = show.actors }
     actors_by_show
+  end
+
+  def shows_by_actor
+   shows_by_actor = {}
+   all_actors.each do |actor|
+    shows_by_actor[actor] = @shows.select { |show| show.actors.include?(actor) }
+   end
+   shows_by_actor
+  end
+
+  def all_actors
+   @shows.flat_map(&:actors).uniq
+  end
+
+  def prolific_actors
+    shows_by_actor.keys.select { |actor| shows_by_actor[actor].count > 1 }
   end
   
 end
--- 1module/exercises/tv_network_2005/test/network_test.rb
+++ 1module/exercises/tv_network_2005/test/network_test.rb
@@ -13,6 +13,8 @@ class NetworkTest < Minitest::Test
     @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
     @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
     @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
+    @mitch = Character.new({name: "Mitch Buchannon", actor: "David Hasselhoff", salary: 1_200_000}) 
+    @baywatch = Show.new("Baywatch", "Gregory Bonann", [@mitch])
     @nbc = Network.new("NBC")  
   end
 
@@ -43,12 +45,32 @@ class NetworkTest < Minitest::Test
   def test_it_returns_actors_by_show
     @nbc.add_show(@knight_rider)
     @nbc.add_show(@parks_and_rec) 
-    # binding.pry
     expected = {
       @knight_rider => ["David Hasselhoff", "William Daniels"],
       @parks_and_rec => ["Amy Poehler", "Nick Offerman"]
     }
     assert_equal expected, @nbc.actors_by_show
+  end
+
+  def test_it_returns_show_by_actor
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@baywatch) 
+    @nbc.add_show(@parks_and_rec)
+    expected = {
+                "David Hasselhoff" => [@knight_rider, @baywatch],  
+                "William Daniels" => [@knight_rider],
+                "Amy Poehler" => [@parks_and_rec],
+                "Nick Offerman" => [@parks_and_rec]
+                }
+     assert_equal expected, @nbc.shows_by_actor
+  end
+
+  def test_it_returns_profilic_actors 
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@baywatch) 
+    @nbc.add_show(@parks_and_rec)
+
+    assert_equal ["David Hasselhoff"], @nbc.prolific_actors
   end
 
 
