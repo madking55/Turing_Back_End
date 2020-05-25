e988dad46c7d5d8ae28a21c01b7756d779d79554
madking55 <madking55@gmail.com>
2020-05-25 22:36:44 +0200

    Finish Iteration 3

--- 1module/assesments/reunion/lib/reunion.rb
+++ 1module/assesments/reunion/lib/reunion.rb
@@ -8,4 +8,21 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+
+  def total_cost
+    @activities.reduce(0) { |sum, activity| sum += activity.total_cost }
+  end
+
+  def breakout
+    hash = Hash.new(0)
+    @activities.each do |activity| 
+      activity.owed.each { |key, value| hash[key] += value }
+    end
+    hash
+  end
+
+  def summary
+    breakout.map { |person, owed| "#{person}: #{owed}\n" }.join.chomp
+  end
+
 end
--- 1module/assesments/reunion/test/reunion_test.rb
+++ 1module/assesments/reunion/test/reunion_test.rb
@@ -6,6 +6,7 @@ class ReunionTest < Minitest::Test
   def setup
     @reunion = Reunion.new("1406 BE")
     @activity_1 = Activity.new("Brunch")
+    @activity_2 = Activity.new("Drinks")
   end
 
   def test_it_exists
@@ -21,5 +22,39 @@ class ReunionTest < Minitest::Test
     assert_equal [], @reunion.activities
     @reunion.add_activity(@activity_1)
     assert_equal [@activity_1], @reunion.activities
+  end
+
+  def test_it_returns_breakout_costs
+    @activity_1.add_participant("Maria", 20)
+    @activity_1.add_participant("Luther", 40)
+    @reunion.add_activity(@activity_1)
+
+    assert_equal 60, @reunion.total_cost
+    
+    @activity_2.add_participant("Maria", 60)
+    @activity_2.add_participant("Luther", 60)
+    @activity_2.add_participant("Louis", 0)
+    @reunion.add_activity(@activity_2)
+    
+    assert_equal 180, @reunion.total_cost
+    expected = {
+                "Maria" => -10, 
+                "Luther" => -30, 
+                "Louis" => 40
+                }
+    assert_equal expected, @reunion.breakout
+  end
+
+  def test_it_returns_summary
+    @activity_1.add_participant("Maria", 20)
+    @activity_1.add_participant("Luther", 40)
+    @reunion.add_activity(@activity_1)
+    @activity_2.add_participant("Maria", 60)
+    @activity_2.add_participant("Luther", 60)
+    @activity_2.add_participant("Louis", 0)
+    @reunion.add_activity(@activity_2)
+    summary = "Maria: -10\nLuther: -30\nLouis: 40"
+    assert_equal summary, @reunion.summary
+    assert_instance_of String, @reunion.summary
   end
 end
