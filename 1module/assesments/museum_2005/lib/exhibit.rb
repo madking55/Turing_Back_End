5edbeece9df8a97d3df8eff0a30be1eab2525f0e
madking55 <madking55@gmail.com>
2020-05-28 19:12:11 +0200

    Finish Iteration 1

---
+++ 1module/assesments/museum_2005/lib/exhibit.rb
@@ -0,0 +1,7 @@
+class Exhibit
+attr_reader :name, :cost
+  def initialize(exhibit_params)
+    @name = exhibit_params[:name]
+    @cost = exhibit_params[:cost]
+  end
+end
---
+++ 1module/assesments/museum_2005/test/patron_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/patron'
+
+class PatronTest < Minitest::Test 
+
+  def setup
+    @patron_1 = Patron.new("Bob", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Patron, @patron_1
+  end
+
+  def test_it_has_attributes
+    assert_equal "Bob", @patron_1.name
+    assert_equal 20, @patron_1.spending_money
+  end
+
+  def test_it_adds_interests
+    assert_equal [], @patron_1.interests
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron_1.interests
+  end
+end
