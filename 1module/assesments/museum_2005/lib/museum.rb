0ee953a6371efddb9b1dda5ffa9f46b3cb45fbfd
madking55 <madking55@gmail.com>
2020-05-28 22:17:51 +0200

    Finish Iteration 3

--- 1module/assesments/museum_2005/lib/museum.rb
+++ 1module/assesments/museum_2005/lib/museum.rb
@@ -1,8 +1,9 @@
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -11,5 +12,37 @@ class Museum
 
   def recommend_exhibits(patron)
    @exhibits.select { |exhibit| patron.interests.include?(exhibit.name)}
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    hash = Hash.new 
+    @exhibits.each do |ex| 
+      hash[ex] = [] 
+      @patrons.each do |patron|
+        hash[ex] << patron if patron.interests.include?(ex.name)
+      end
+    end
+    hash
+  end
+
+  def ticket_lottery_contestants(exhibit)
+    patrons_by_exhibit_interest[exhibit].select { |patron| exhibit.cost > patron.spending_money }
+  end
+
+  def draw_lottery_winner(exhibit)
+    winner = ticket_lottery_contestants(exhibit).sample
+    winner.name unless winner.nil?
+  end
+
+  def announce_lottery_winner(exhibit)
+    if draw_lottery_winner(exhibit).nil?
+      "No winners for this lottery"
+    else
+      "#{draw_lottery_winner(exhibit)} has won the #{exhibit.name} exhibit lottery"
+    end
   end
 end
--- 1module/assesments/museum_2005/test/museum_test.rb
+++ 1module/assesments/museum_2005/test/museum_test.rb
@@ -1,8 +1,11 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/museum'
 require './lib/patron'
 require './lib/exhibit'
+require 'pry'
+
 
 class MuseumTest < Minitest::Test
   def setup
@@ -10,8 +13,22 @@ class MuseumTest < Minitest::Test
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
-    @patron_1 = Patron.new("Bob", 20)
+    @patron_1 = Patron.new("Bob", 0)
     @patron_2 = Patron.new("Sally", 20)
+    @patron_3 = Patron.new("Johnny", 5)
+  end
+
+  def setup_museum
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
   end
 
   def test_it_exists
@@ -42,4 +59,38 @@ class MuseumTest < Minitest::Test
     assert_equal [@gems_and_minerals, @dead_sea_scrolls] , @dmns.recommend_exhibits(@patron_1)
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
+
+  def test_it_returns_patrons_by_exhibit_interest
+    setup_museum
+
+    assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
+    expected = {
+      @gems_and_minerals => [@patron_1],
+      @dead_sea_scrolls => [@patron_1, @patron_2, @patron_3],
+      @imax => []
+    }
+    assert_equal expected, @dmns.patrons_by_exhibit_interest
+  end
+
+  def test_it_returns_ticket_lottery_contestants
+    setup_museum
+
+    assert_equal [@patron_1, @patron_3], @dmns.ticket_lottery_contestants(@dead_sea_scrolls)
+  end
+
+  def test_it_draws_lottery_winner
+    setup_museum
+
+    assert_nil @dmns.draw_lottery_winner(@gems_and_minerals)
+    @dmns.stubs(:draw_lottery_winner).returns("Johny")
+    assert_equal "Johny", @dmns.draw_lottery_winner(@dead_sea_scrolls)
+  end
+
+  def test_it_announces_lottery_winner
+    setup_museum
+    assert_equal "No winners for this lottery", @dmns.announce_lottery_winner(@imax)
+    @dmns.stubs(:draw_lottery_winner).returns("Bob")
+    assert_equal "Bob has won the Dead Sea Scrolls exhibit lottery", @dmns.announce_lottery_winner(@dead_sea_scrolls)
+  end
+
 end
