78cca6e0a6c977ed55136f1885171236ee63fc80
madking55 <madking55@gmail.com>
2020-06-02 21:29:56 +0200

    Finish Iteration 1

---
+++ 1module/assesments/food_truck_event_2003/test/food_truck_test.rb
@@ -0,0 +1,35 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/food_truck'
+require './lib/item'
+
+class FoodTruckTest < Minitest::Test
+  
+  def setup
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+  end
+
+  def test_it_exists
+    assert_instance_of FoodTruck, @food_truck
+  end
+
+  def test_it_has_attributes
+    assert_equal "Rocky Mountain Pies", @food_truck.name
+    assert_equal ({}), @food_truck.inventory
+  end
+
+  def test_it_returns_stock_items
+    assert_equal 0, @food_truck.check_stock(@item1)
+    @food_truck.stock(@item1, 30)
+    assert_equal 30, @food_truck.check_stock(@item1)
+    assert_equal ({ @item1 => 30 }), @food_truck.inventory
+    
+    @food_truck.stock(@item1, 25)
+    assert_equal ({ @item1 => 55 }), @food_truck.inventory
+    
+    @food_truck.stock(@item2, 12)
+    assert_equal ({ @item1 => 55, @item2 => 12 }), @food_truck.inventory
+  end
+end
