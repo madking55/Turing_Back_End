d663a23221929c40d7ac0962296296e631491631
madking55 <madking55@gmail.com>
2020-06-03 11:55:46 +0200

    Finish Iteration 3

--- 1module/assesments/food_truck_event_2003/lib/event.rb
+++ 1module/assesments/food_truck_event_2003/lib/event.rb
@@ -16,4 +16,26 @@ class Event
   def food_trucks_that_sell(item)
     @food_trucks.select { |truck| truck.inventory[item] != 0 }
   end
+
+  def total_inventory
+    total_inventory = Hash.new { |hash, key| hash[key] = { quantity: 0, food_trucks: [] } }
+    @food_trucks.each do |truck|
+      truck.inventory.each do |item, q|
+        total_inventory[item][:quantity] += q
+        total_inventory[item][:food_trucks] << truck
+      end
+    end
+    total_inventory
+  end
+
+  def sorted_item_list
+    total_inventory.keys.map(&:name).sort
+  end
+
+  def overstocked_items
+    overstocked_items = total_inventory.select do |item, hash| 
+     hash[:quantity] > 50 && hash[:food_trucks].size > 1
+    end
+    overstocked_items.keys
+  end
 end
--- 1module/assesments/food_truck_event_2003/test/event_test.rb
+++ 1module/assesments/food_truck_event_2003/test/event_test.rb
@@ -17,12 +17,16 @@ class EventTest < Minitest::Test
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
-  def setup_stock
+  def setup_event
     @food_truck1.stock(@item1, 35)   
     @food_truck1.stock(@item2, 7) 
     @food_truck2.stock(@item4, 50)  
     @food_truck2.stock(@item3, 25)
-    @food_truck3.stock(@item1, 65)  
+    @food_truck3.stock(@item1, 65) 
+    @food_truck3.stock(@item3, 10)   
+    @event.add_food_truck(@food_truck1) 
+    @event.add_food_truck(@food_truck2) 
+    @event.add_food_truck(@food_truck3) 
   end
 
   def test_it_exists
@@ -35,30 +39,55 @@ class EventTest < Minitest::Test
   end
 
   def test_it_adds_food_trucks
-    setup_stock
-    @event.add_food_truck(@food_truck1) 
-    @event.add_food_truck(@food_truck2) 
-    @event.add_food_truck(@food_truck3) 
+    setup_event
     assert_equal [@food_truck1, @food_truck2, @food_truck3], @event.food_trucks
   end
 
   def test_it_return_food_truck_names
-    setup_stock
-    @event.add_food_truck(@food_truck1) 
-    @event.add_food_truck(@food_truck2) 
-    @event.add_food_truck(@food_truck3) 
+    setup_event
     expected = ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
     assert_equal expected, @event.food_truck_names
   end
 
   def test_it_returns_trucks_that_sell_item
-    setup_stock
-    @event.add_food_truck(@food_truck1) 
-    @event.add_food_truck(@food_truck2) 
-    @event.add_food_truck(@food_truck3) 
-
+    setup_event
     assert_equal [@food_truck1, @food_truck3], @event.food_trucks_that_sell(@item1)
     assert_equal [@food_truck2], @event.food_trucks_that_sell(@item4)
+  end
+
+  def test_it_returns_sorted_item_list
+    setup_event
+    expected = ["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"]
+    assert_equal expected, @event.sorted_item_list
+  end
+
+  def test_it_returns_total_inventory
+    setup_event
+    expected = {
+      @item1 => {
+        quantity: 100,
+        food_trucks: [@food_truck1, @food_truck3]
+      },
+      @item2 => {
+        quantity: 7,
+        food_trucks: [@food_truck1]
+      },
+      @item3 => {
+        quantity: 35,
+        food_trucks: [@food_truck2, @food_truck3]
+      },
+      @item4 => {
+        quantity: 50,
+        food_trucks: [@food_truck2]
+      }
+    }
+
+    assert_equal expected, @event.total_inventory
+  end
+
+  def test_it_returns_overstocked_items
+    setup_event
+    assert_equal [@item1], @event.overstocked_items
   end
 
 end
