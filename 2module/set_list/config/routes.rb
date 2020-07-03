41dba55b433fce5011ad130c8c1dbd2ed92d5c6e
madking55 <madking55@gmail.com>
2020-07-03 18:09:00 +0200

    Add destroying an artist

--- 2module/set_list/config/routes.rb
+++ 2module/set_list/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
-  get 'artists', to: 'artists#index'
-  get 'artists/new', to: 'artists#new'
-  post 'artists', to: 'artists#create'
+  get '/artists', to: 'artists#index'
+  get '/artists/new', to: 'artists#new'
+  post '/artists', to: 'artists#create'
+  delete '/artists/:id', to: 'artists#destroy'
 end
  
