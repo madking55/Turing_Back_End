db67a30cb4380414cb736df8b2ea1e1efc861eb2
madking55 <madking55@gmail.com>
2020-07-03 16:49:27 +0200

    Add New Artist form

---
+++ 2module/set_list/spec/features/artists/new_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'New Artist' do 
+  describe 'as a visitor' do 
+    describe 'When I visit the new artist form by clicking a link on the index' do
+      it 'I can create new artist' do 
+        visit '/artists'
+        click_link 'New Artist'
+
+        expect(current_path).to eq('/artists/new') 
+
+        fill_in 'Name', with: 'Megan'
+        click_on 'Create Artist'
+
+        expect(current_path).to eq('/artists')
+        expect(page).to have_content('Megan')
+      end
+    end
+  end
+end
---
+++ 2module/set_list/spec/features/songs/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'songs index page', type: :feature do 
+  it 'can see all songs titles and play count' do 
+    carly_rae = Artist.create(name: 'Carly Rae')
+    prince = Artist.create(name: 'Prince')
+    # Different ways to associate songs with artists:
+    song_1 = Song.create(title: 'I Really Like You', length: 208, play_count: 243810867, artist: carly_rae)
+    song_2 = Song.create(title: 'Call Me Maybe', length: 199, play_count: 1214722172, artist_id: carly_rae.id)
+    song_3 = prince.songs.create(title: 'Purple Rain', length: 300, play_count: 1467408565)
+
+    visit '/songs'
+    expect(page).to have_content(song_1.title) 
+    expect(page).to have_content("Play Count: #{song_1.play_count}")
+
+    expect(page).to have_content(song_3.title) 
+    expect(page).to have_content("Play Count: #{song_3.play_count}")
+  end
+end
