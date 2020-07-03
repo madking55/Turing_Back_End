8f17acbcf7154442e13a9d46f4dd9f3b6e8dc306
madking55 <madking55@gmail.com>
2020-07-03 15:36:31 +0200

    Add class and instance methods do models

--- 2module/set_list/app/models/song.rb
+++ 2module/set_list/app/models/song.rb
@@ -1,4 +1,8 @@
 class Song < ApplicationRecord
   validates_presence_of :title
-  belongs_to :artist
+  belongs_to :artist # now artist is a method you can call on Song
+
+  def self.song_count
+    Song.all.count
+  end
 end
