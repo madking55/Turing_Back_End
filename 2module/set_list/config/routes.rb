d144b73cb1d49712b86881cf0c79d2e23d0ab68a
madking55 <madking55@gmail.com>
2020-07-11 12:02:29 +0200

    Add Playlist index page

--- 2module/set_list/config/routes.rb
+++ 2module/set_list/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
+
   get '/artists', to: 'artists#index'
   get '/artists/new', to: 'artists#new'
   post '/artists', to: 'artists#create'
@@ -10,5 +11,7 @@ Rails.application.routes.draw do
   get '/artists/:artist_id/songs/new', to: 'songs#new'
   post '/artists/:artist_id/songs', to: 'songs#create'
   get '/artists/:artist_id/songs', to: 'songs#songs_by_artist'
+
+  get '/playlists', to: 'playlists#index'
 end
  
