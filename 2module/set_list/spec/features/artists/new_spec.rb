8aedbd913b78a3751be02f37f06d4394f57b1da9
madking55 <madking55@gmail.com>
2020-07-13 17:29:32 +0200

    Add flash messages for creating new artist sad path

--- 2module/set_list/spec/features/artists/new_spec.rb
+++ 2module/set_list/spec/features/artists/new_spec.rb
@@ -18,6 +18,14 @@ RSpec.describe 'New Artist' do
         expect(current_path).to eq('/artists')
         expect(page).to have_content('Prince')
       end
+
+      it 'I can not create an artist without a name' do
+        visit '/artists/new'
+        click_on 'Create Artist'
+
+        expect(page).to have_content('Artist not created: Required information missing.')
+        expect(page).to have_button('Create Artist')
+      end
     end
   end
 end
