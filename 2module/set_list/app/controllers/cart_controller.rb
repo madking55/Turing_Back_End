3c1105e08b34e6dd8ace18b62a14934114e9e2d7
madking55 <madking55@gmail.com>
2020-07-16 15:30:04 +0200

    Add cart tracker

--- 2module/set_list/app/controllers/cart_controller.rb
+++ 2module/set_list/app/controllers/cart_controller.rb
@@ -3,11 +3,10 @@ class CartController < ApplicationController
 
   def update
     song = Song.find(params[:song_id])
+    cart.add_song(song.id)
     song_id_str = song.id.to_s
-    session[:cart] ||= Hash.new(0)
-    session[:cart][song_id_str] ||= 0
-    session[:cart][song_id_str] = session[:cart][song_id_str] + 1
-    quantity = session[:cart][song_id_str]
+    session[:cart] = cart.contents
+    quantity = cart.count_of(song.id)
     flash[:notice] = "You now have #{pluralize(quantity, "copy")} of #{song.title} in your cart."
     redirect_to "/songs"
   end
--- 2module/set_list/spec/features/cart/add_song_spec.rb
+++ 2module/set_list/spec/features/cart/add_song_spec.rb
@@ -35,4 +35,32 @@ RSpec.describe "When a user adds songs to their cart" do
 
     expect(page).to have_content("You now have 2 copies of #{song_1.title} in your cart.")
   end
+
+  it 'displays the total number of songs in the cart' do
+    artist = Artist.create(name: 'Rick Astley')
+    song_1 = artist.songs.create(title: 'Never Gonna Give You Up', length: 250, play_count: 1000000)
+    song_2 = artist.songs.create(title: "Don't Stop Believin'", length: 300, play_count: 1)
+
+    visit '/songs'
+
+    expect(page).to have_content("Cart: 0")
+
+    within("#song-#{song_1.id}") do
+      click_button 'Add Song'
+    end
+
+    expect(page).to have_content("Cart: 1")
+
+    within("#song-#{song_2.id}") do
+      click_button 'Add Song'
+    end
+
+    expect(page).to have_content("Cart: 2")
+
+    within("#song-#{song_1.id}") do
+      click_button 'Add Song'
+    end
+
+    expect(page).to have_content("Cart: 3")
+  end
 end
