c82dd1431d013988016a9da80c179866ac13dba5
madking55 <madking55@gmail.com>
2020-07-16 14:09:43 +0200

    Add multiple songs in cart

--- 2module/set_list/app/controllers/cart_controller.rb
+++ 2module/set_list/app/controllers/cart_controller.rb
@@ -1,7 +1,14 @@
 class CartController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
   def update
     song = Song.find(params[:song_id])
-    flash[:notice] = "You now have 1 copy of #{song.title} in your cart."
-    redirect_to '/songs'
+    song_id_str = song.id.to_s
+    session[:cart] ||= Hash.new(0)
+    session[:cart][song_id_str] ||= 0
+    session[:cart][song_id_str] = session[:cart][song_id_str] + 1
+    quantity = session[:cart][song_id_str]
+    flash[:notice] = "You now have #{pluralize(quantity, "copy")} of #{song.title} in your cart."
+    redirect_to "/songs"
   end
 end
--- 2module/set_list/spec/features/cart/add_song_spec.rb
+++ 2module/set_list/spec/features/cart/add_song_spec.rb
@@ -1,16 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "When a user adds songs to their cart" do
-  it "displays a message" do
+  it 'displays a message' do
     artist = Artist.create(name: 'Rick Astley')
     song = artist.songs.create(title: 'Never Gonna Give You Up', length: 250, play_count: 1000000)
 
     visit "/songs"
 
     within("#song-#{song.id}") do
-      click_button "Add Song"
+      click_button 'Add Song'
     end
 
     expect(page).to have_content("You now have 1 copy of #{song.title} in your cart.")
+  end
+
+  it 'increments the messagge for multiple songs' do
+    artist = Artist.create(name: 'Rick Astley')
+    song_1 = artist.songs.create(title: 'Never Gonna Give You Up', length: 250, play_count: 1000000)
+    song_2 = artist.songs.create(title: "Don't Stop Believin'", length: 300, play_count: 1)
+
+    visit '/songs'
+
+    within("#song-#{song_1.id}") do
+      click_button 'Add Song'
+    end
+
+    within("#song-#{song_2.id}") do
+      click_button 'Add Song'
+    end
+
+    within("#song-#{song_1.id}") do
+      click_button 'Add Song'
+    end
+
+    expect(page).to have_content("You now have 2 copies of #{song_1.title} in your cart.")
   end
 end
