2d1e1b13e6aec5d5550f6071c123f566340417fd
madking55 <madking55@gmail.com>
2020-07-27 18:40:43 +0200

    Initial commit

---
+++ 2module/apollo_14/spec/models/astronaut_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Astronaut, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :age }
+    it { should validate_presence_of :job }
+  end
+
+  describe 'Relationships' do
+    it { should have_many :astronaut_missions}
+    it { should have_many :missions}
+  end
+end
