bbc31b25f44309a9ffeee706a48e90473801f709
madking55 <madking55@gmail.com>
2020-10-14 14:13:33 +0200

    Finish Consuming API Lesson

---
+++ 3module/house-salad/app/controllers/search_controller.rb
@@ -0,0 +1,13 @@
+class SearchController < ApplicationController
+  def index
+    state = params[:state]
+    url = "https://api.propublica.org"
+    conn = Faraday.new(url) do |faraday|
+      faraday.headers['X-API-Key'] =  ENV["PROPUBLICA_API_KEY"]
+    end
+
+    response = conn.get("/congress/v1/members/house/#{state}/current.json")
+    json = JSON.parse(response.body, symbolize_names: true)
+    @members = json[:results]
+  end
+end
