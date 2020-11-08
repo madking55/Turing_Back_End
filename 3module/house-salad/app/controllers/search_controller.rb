39bd0d39224957638e573d3ac79acbd0abd2301b
madking55 <madking55@gmail.com>
2020-11-08 12:19:50 +0100

    Refactor Lesson p.1 - Add Member PORO class

--- 3module/house-salad/app/controllers/search_controller.rb
+++ 3module/house-salad/app/controllers/search_controller.rb
@@ -3,11 +3,14 @@ class SearchController < ApplicationController
     state = params[:state]
     url = "https://api.propublica.org"
     conn = Faraday.new(url) do |faraday|
-      faraday.headers['X-API-Key'] =  ENV["PROPUBLICA_API_KEY"]
+      faraday.headers['X-API-Key'] = ENV["PROPUBLICA_API_KEY"]
     end
 
     response = conn.get("/congress/v1/members/house/#{state}/current.json")
     json = JSON.parse(response.body, symbolize_names: true)
-    @members = json[:results]
+    @members = json[:results].map do |member_data|
+      # binding.pry
+      Member.new(member_data)
+    end
   end
 end
