4b1dcb22360d97a1a90841479c7ead1a7ee8ed3d
madking55 <madking55@gmail.com>
2020-12-17 12:36:37 +0100

    Refactor Track specs with factories

--- 2module/factory_girl_intro/spec/models/track_spec.rb
+++ 2module/factory_girl_intro/spec/models/track_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Track do
   describe "attributes" do
     it "has a name, composer, and millisecond amount" do
-      track = Track.new
+      track = build(:track)
 
       expect(track).to respond_to(:name)
       expect(track).to respond_to(:composer)
@@ -22,39 +22,28 @@ RSpec.describe Track do
     it { is_expected.to have_many(:playlists).through(:playlist_tracks) }
 
     it "is associated with a genre" do
-      genre = Genre.create(name: "R&B")
-      track = Track.new(genre: genre)
+      track = build(:track)
 
-      expect(track.genre.name).to eq "R&B"
+      expect(track.genre).not_to be nil
     end
 
     it "is on an album" do
-      album = Album.create(title: "Ring Away")
-      track = Track.new(album: album)
+      track = build(:track)
 
-      expect(track.album.title).to eq "Ring Away"
+      expect(track.album).not_to be nil
     end
 
     it "has a media type" do
-      media_type = MediaType.create(name: "mp3")
-      track = Track.new(media_type: media_type)
-
-      expect(track.media_type.name).to eq "mp3"
+      track = build(:track)
+ 
+      expect(track.media_type).not_to be nil
     end
 
     it "can be on many playlists" do
-      album = Album.create(title: "Ring Away")
-      genre = Genre.create(name: "R&B")
-      media_type = MediaType.create(name: "mp3")
-      track = Track.create(
-        name: "Big Poppa", 
-        genre: genre,
-        album: album,
-        media_type: media_type,
-        milliseconds: 10
-      )
-      playlist1 = Playlist.create(name: "Roundies")
-      playlist2 = Playlist.create(name: "Bouties")
+      track = create(:track)
+
+      playlist1 = create(:playlist)
+      playlist2 = create(:playlist)
 
       playlist1.tracks << track
       playlist2.tracks << track
