8bddc36af3dc3884c61e214ea18d18d25b78fee5
madking55 <madking55@gmail.com>
2020-12-17 11:45:57 +0100

    Refactor Genre specs with factories

--- 2module/factory_girl_intro/spec/models/genre_spec.rb
+++ 2module/factory_girl_intro/spec/models/genre_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Genre do
   describe "attributes" do
     it "has a name" do
-      genre = Genre.new(name: "folk")
+      genre = build(:genre, name: 'folk')
 
       expect(genre).to respond_to(:name)
       expect(genre.name). to eq "folk"
@@ -16,44 +16,14 @@ RSpec.describe Genre do
 
   describe "associations" do
     it "has many different tracks" do
-      media_type = MediaType.create(name: "LazerDisk")
-      album      = Album.create(title: "Ring Away")
-      genre      = Genre.create(name: "R&B")
-      genre2     = Genre.create(name: "Jazz")
+      genre = create(:genre)
+      genre2 = create(:genre, name: "Jazz")
 
-      track1 = Track.create(
-        name: "Big Poppa", 
-        genre: genre,
-        album: album,
-        media_type: media_type,
-        milliseconds: 10
-      )
-      track2 = Track.create(
-        name: "Pig Boppa", 
-        genre: genre,
-        album: album,
-        media_type: media_type,
-        milliseconds: 10
-      )
-      track3 = Track.create(
-        name: "Small Father", 
-        genre: genre,
-        album: album,
-        media_type: media_type,
-        milliseconds: 10
-      )
-
-      Track.create(
-        name: "Medium Mother", 
-        genre: genre2,
-        album: album,
-        media_type: media_type,
-        milliseconds: 10
-      )
+      create_list(:track, 3, genre: genre)
+      create(:track, genre: genre2)
 
       expect(Track.count).to eq 4
       expect(genre.tracks.count).to eq 3
-      expect(genre.tracks.last.name).to eq "Small Father"
     end
   end
 end
